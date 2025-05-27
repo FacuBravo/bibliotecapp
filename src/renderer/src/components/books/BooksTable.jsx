f9ac8341eb69749984895e66194eeaf06023f13e
Facundo Bravo <bfacundo176@gmail.com>
2025-05-27 00:14:45 -0300

    feat: implement BooksTable component in BooksPage

---
+++ src/renderer/src/components/books/BooksTable.jsx
@@ -0,0 +1,99 @@
+import { useEffect } from 'react'
+
+import { useAuthStore, useBooksStore } from '../../hooks'
+import arrowIcon from '../../assets/images/icons/Arrow.svg'
+import infoIcon from '../../assets/images/icons/Info.svg'
+import trashIcon from '../../assets/images/icons/Trash.svg'
+
+export const BooksTable = () => {
+    const { books, startLoadingBooks } = useBooksStore()
+    const { user } = useAuthStore()
+
+    useEffect(() => {
+        startLoadingBooks()
+    }, [])
+
+    return (
+        <table className="flex h-full w-4/5 flex-col gap-6">
+            <thead className="font-supermercado text-xl">
+                <tr className="flex items-center rounded-2xl bg-yellow_500 px-6 py-4 text-yellow_600 shadow-md transition-all">
+                    <td className="flex w-[5%] cursor-pointer items-center">
+                        #
+                        <img
+                            className="sort_img"
+                            id="arrow_order_by_n"
+                            src={arrowIcon}
+                            alt="Arrow"
+                        />
+                    </td>
+
+                    <td className="flex w-[27%] cursor-pointer items-center">
+                        Título
+                        <img
+                            className="sort_img hidden"
+                            id="arrow_order_by_title"
+                            src={arrowIcon}
+                            alt="Arrow"
+                        />
+                    </td>
+
+                    <td className="flex w-[27%] cursor-pointer items-center">
+                        Autor
+                        <img
+                            className="sort_img hidden"
+                            id="arrow_order_by_author"
+                            src={arrowIcon}
+                            alt="Arrow"
+                        />
+                    </td>
+
+                    <td className="flex w-[27%] cursor-pointer items-center">
+                        Tema
+                        <img
+                            className="sort_img hidden"
+                            id="arrow_order_by_theme"
+                            src={arrowIcon}
+                            alt="Arrow"
+                        />
+                    </td>
+
+                    <td className="w-[14%] items-center justify-end text-end">Acciones</td>
+                </tr>
+            </thead>
+
+            <tbody className="flex flex-col gap-6 font-assistant text-lg">
+                {books.map((book, index) => (
+                    <tr
+                        key={book.id}
+                        className={`${index % 2 === 0 ? 'bg-yellow_400' : 'bg-yellow_500'} flex items-center rounded-2xl px-6 py-4 text-yellow_600 shadow-md transition-all`}
+                    >
+                        <td className="w-[5%]">{book.inventory}</td>
+                        <td className="w-[27%]">{book.title}</td>
+                        <td className="w-[27%]">{book.author}</td>
+                        <td className="w-[27%]">{book.theme}</td>
+                        <td className="flex w-[14%] items-center justify-end gap-2 text-end">
+                            <button className="cursor-pointer bg-transparent">
+                                <img src={infoIcon} alt="See more" />
+                            </button>
+
+                            {user.sessionToken && (
+                                <>
+                                    <button className="cursor-pointer bg-transparent">
+                                        <img src={trashIcon} alt="Delete" />
+                                    </button>
+
+                                    {book.borrowed === 0 && (
+                                        <button className="cursor-pointer rounded-lg bg-orange_600 p-2 text-white">
+                                            Prestar
+                                        </button>
+                                    )}
+                                </>
+                            )}
+                            {book.borrowed == 1 && <p>Prestado</p>}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
