a34b3884010bf46d3ee0f76c173931cf5798d979
Facundo Bravo <bfacundo176@gmail.com>
2025-06-08 19:27:50 -0300

    feat: show error alerts

--- src/renderer/src/BibliotecApp.jsx
+++ src/renderer/src/BibliotecApp.jsx
@@ -1,15 +1,30 @@
 import { useEffect } from 'react'
 import { Header, LoginModal, RegisterModal, Alert, ConfirmModal } from './components/commons'
 import { AppRouter } from './routers/AppRouter'
-import { useAuthStore, useUiStore } from './hooks'
+import { useAuthStore, useBooksStore, useLoansStore, usePartnersStore, useUiStore } from './hooks'
 
 export const BibliotecApp = () => {
-    const { errorMessage } = useAuthStore()
+    const { errorMessage: authError } = useAuthStore()
+    const { error: booksError } = useBooksStore()
+    const { error: partnersError } = usePartnersStore()
+    const { error: loansError } = useLoansStore()
     const { onShowAlert } = useUiStore()
 
     useEffect(() => {
-        if (errorMessage) onShowAlert(errorMessage, 'error')
-    }, [errorMessage])
+        if (authError) onShowAlert(authError, 'error')
+    }, [authError])
+
+    useEffect(() => {
+        if (booksError) onShowAlert(booksError, 'error')
+    }, [booksError])
+
+    useEffect(() => {
+        if (partnersError) onShowAlert(partnersError, 'error')
+    }, [partnersError])
+
+    useEffect(() => {
+        if (loansError) onShowAlert(loansError, 'error')
+    }, [loansError])
 
     return (
         <>
